1d89d5db036d232c6f77a992ee4d69a091f6a633
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-05-05 14:57:30 +0200

    added authentification frontend

--- src/components/form/AppFormInput.tsx
+++ src/components/form/AppFormInput.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const AppFormInput = styled.input`
+const FormInputContainer = styled.div`
+  position: relative;
+`;
+
+const FormInput = styled.input`
   padding: 1rem;
   color: ${(props) => props.theme.white};
   border-bottom: 1px solid ${(props) => props.theme.greyishBlue};
@@ -8,5 +13,26 @@ const AppFormInput = styled.input`
   font-weight: 300;
   width: 100%;
 `;
+
+const FormErrorMessage = styled.p`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 1rem;
+`;
+
+interface IAppFormInput {
+  placeholder: string;
+}
+
+const AppFormInput = ({ placeholder }: IAppFormInput) => {
+  const [isEmpty, setIsEmpty] = useState(false);
+  return (
+    <FormInputContainer>
+      <FormInput placeholder={placeholder} />
+      {isEmpty && <FormErrorMessage>Can't be empty</FormErrorMessage>}
+    </FormInputContainer>
+  );
+};
 
 export default AppFormInput;
